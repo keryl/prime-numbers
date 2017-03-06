46c672dac8f27ab3b0ab1e2ed58eca913799fb9f
keryl <kerylklonye@gmail.com>
2017-03-06 21:02:50 +0300

    Implement is_prime_number

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,5 +1,12 @@
 def is_prime_number(num):
-    pass
+
+    # loop through all numbers less `than` num and return true if it is not divisible by any of them
+
+    for x in range(2, num):
+        if num % x == 0:
+            return False
+    else:
+        return True
 
 def get_prime_numbers(n):
     pass
