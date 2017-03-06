1124e3681eb81a88c77bd5f1a360bf7c3feca8b3
keryl <kerylklonye@gmail.com>
2017-03-06 21:03:37 +0300

    is_prime_number should return false if given number is less than 2

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,5 +1,8 @@
 def is_prime_number(num):
 
+    if num < 2: # prime numbers start from 2
+        return False
+
     # loop through all numbers less `than` num and return true if it is not divisible by any of them
 
     for x in range(2, num):
